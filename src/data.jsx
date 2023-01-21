a58206ff9f7278d27689f45ce688f816c2b4d436
nyame100 <nyamerichmond100@gmail.com>
2023-01-21 08:45:53 +0100

    add data.jsx component

---
+++ src/data.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import download from "../src/assets/download.jpeg";
+import download1 from "../src/assets/download1.jpeg";
+import download2 from "../src/assets/download2.jpeg";
+import download3 from "../src/assets/download3.jpeg";
+import download4 from "../src/assets/download4.jpeg";
+import mosh from "../src/assets/mosh.jpg";
+import IMG from "../src/assets/IMG.png";
+
+export const portfolios = [
+  {
+    id: 1,
+    img: download1,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 2,
+    img: download2,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 3,
+    img: download3,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 4,
+    img: download4,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 5,
+    img: download,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 6,
+    img: download,
+    title: "some title",
+    git_url: "https://github.com",
+    demo_url: "https://dribbble.com/Alien_pixels",
+  },
+];
