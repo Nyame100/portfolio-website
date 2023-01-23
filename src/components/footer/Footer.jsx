00390698692a30be5463e2ba5089df8e78e06847
nyame100 <nyamerichmond100@gmail.com>
2023-01-23 08:05:07 +0100

    update footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./footer.css";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo"></a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/nyame-richmond/">
+          <AiFillLinkedin className="socials__icon" />
+        </a>
+        <a href="https://github.com/Nyame100">
+          <AiFillGithub className="socials__icon" />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
