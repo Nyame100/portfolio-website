3577bec651fa82a967f68e97b9689fbc7e1d0923
nyame100 <nyamerichmond100@gmail.com>
2023-02-21 17:16:19 +0100

    update experience component

--- src/data.jsx
+++ src/data.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import download from "../src/assets/download.jpeg";
-import download1 from "../src/assets/download1.jpeg";
+import fabregat from "../src/assets/fabregat.jpg";
 import download2 from "../src/assets/download2.jpeg";
 import download3 from "../src/assets/download3.jpeg";
 import download4 from "../src/assets/download4.jpeg";
 import cocktails from "../src/assets/cocktails2.JPG";
-import IMG from "../src/assets/IMG.png";
+import stockphotos from "../src/assets/stockphotos.JPG";
+import movies from "../src/assets/movies.JPG";
+import joseph from "../src/assets/joseph.jpg";
 
 export const portfolios = [
   {
@@ -17,17 +19,17 @@ export const portfolios = [
   },
   {
     id: 2,
-    img: download2,
+    img: stockphotos,
     title: "Stock-Photo Project",
     git_url: "https://github.com/Nyame100/stock-photos-project",
-    demo_url: "https://stock-photoss.netlify.app/",
+    demo_url: "https://stock-photoss.netlify.app",
   },
   {
     id: 3,
-    img: download3,
-    title: "some title",
-    git_url: "https://github.com",
-    demo_url: "https://dribbble.com/Alien_pixels",
+    img: movies,
+    title: "Movies-DB Project",
+    git_url: "https://github.com/Nyame100/movieDB-project",
+    demo_url: "https://movies-db-1.netlify.app",
   },
   {
     id: 4,
@@ -55,22 +57,22 @@ export const portfolios = [
 export const testimonials = [
   {
     id: 1,
-    name: "Joseph Djaba",
-    img: download3,
+    name: "Xavier Fabregat",
+    img: fabregat,
     comment:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo aut ad distinctio beatae quo dolorem, voluptates consequuntur natus in.",
+      "Richmond is a driven developer. He is motivated to always learn and implement new ideas in projects. He is also a good team player.",
   },
   {
     id: 2,
     name: "Joseph Djaba",
-    img: download3,
+    img: joseph,
     comment:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo aut ad distinctio beatae quo dolorem, voluptates consequuntur natus in.",
+      "Richmond is dedicated to whatever he sets his mind on. He is enthusiastic, commited and creative in finding solutions to problems.",
   },
   {
     id: 3,
     name: "Joseph Djaba",
-    img: download3,
+    img: joseph.jpg,
     comment:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit, rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo aut ad distinctio beatae quo dolorem, voluptates consequuntur natus in.",
   },
